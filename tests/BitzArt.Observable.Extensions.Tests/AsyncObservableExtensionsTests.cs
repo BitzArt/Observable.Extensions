a8641d7ed612cf1355e2f1c72789dcd2d6258038
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2025-04-20 12:33:01 +0400

    Rework tests

---
+++ tests/BitzArt.Observable.Extensions.Tests/AsyncObservableExtensionsTests.cs
@@ -0,0 +1,65 @@
+﻿namespace BitzArt.Observable.Extensions.Tests;
+
+public class AsyncObservableExtensionsTests
+{
+    [Fact]
+    public async Task Subscribe_OnNextCallbackPassed_ShouldStartTrigger()
+    {
+        // Arrange
+        var observable = new AsyncObservable<bool>();
+        var trriggered = false;
+
+        // Act
+        observable.Subscribe(onNext: async (_) =>
+        {
+            trriggered = true;
+            await Task.CompletedTask;
+        });
+
+        // Assert
+        await observable.OnNextAsync(true);
+        Assert.True(trriggered);
+    }
+
+    [Fact]
+    public async Task Subscribe_OnCompletedCallbackPassed_ShouldStartTrigger()
+    {
+        // Arrange
+        var observable = new AsyncObservable<bool>();
+        var triggered = false;
+
+        // Act
+        observable.Subscribe(
+            onCompleted: async () =>
+            {
+                triggered = true;
+                await Task.CompletedTask;
+            },
+            onNext: async (_) => await Task.CompletedTask);
+
+        // Assert
+        await observable.OnCompletedAsync();
+        Assert.True(triggered);
+    }
+
+    [Fact]
+    public async Task Subscribe_OnErrorCallbackPassed_ShouldStartTrigger()
+    {
+        // Arrange
+        var observable = new AsyncObservable<bool>();
+        var triggered = false;
+
+        // Act
+        observable.Subscribe(
+            onError: async (_) =>
+            {
+                triggered = true;
+                await Task.CompletedTask;
+            },
+            onNext: async (_) => await Task.CompletedTask);
+
+        // Assert
+        await observable.OnErrorAsync(new Exception());
+        Assert.True(triggered);
+    }
+}
---
+++ tests/BitzArt.Observable.Extensions.Tests/ObservableExtensionsTests.cs
@@ -0,0 +1,62 @@
+﻿namespace BitzArt.Observable.Extensions.Tests;
+
+public class ObservableExtensionsTests
+{
+    [Fact]
+    public async Task Subscribe_OnNextCallbackPassed_ShouldStartTrigger()
+    {
+        // Arrange
+        var observable = new AsyncObservable<bool>();
+        var trriggered = false;
+
+        // Act
+        observable.Subscribe(onNext: (_) =>
+        {
+            trriggered = true;
+        });
+
+        // Assert
+        await observable.OnNextAsync(true);
+        Assert.True(trriggered);
+    }
+
+    [Fact]
+    public async Task Subscribe_OnCompletedCallbackPassed_ShouldStartTrigger()
+    {
+        // Arrange
+        var observable = new AsyncObservable<bool>();
+        var triggered = false;
+
+        // Act
+        observable.Subscribe(
+            onCompleted: () =>
+            {
+                triggered = true;
+            },
+            onNext: (_) => { });
+
+        // Assert
+        await observable.OnCompletedAsync();
+        Assert.True(triggered);
+    }
+
+    [Fact]
+    public async Task Subscribe_OnErrorCallbackPassed_ShouldStartTrigger()
+    {
+        // Arrange
+        var observable = new AsyncObservable<bool>();
+        var triggered = false;
+
+        // Act
+        observable.Subscribe(
+            onError: (_) =>
+            {
+                triggered = true;
+            },
+            onNext: (_) => { });
+
+        // Assert
+        await observable.OnErrorAsync(new Exception());
+        Assert.True(triggered);
+    }
+}
